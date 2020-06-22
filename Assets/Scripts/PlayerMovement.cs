4c6190840c4a94116f45b7d4ac30fe853a6cefeb
José F <josevictorjop@gmail.com>
2020-06-21 21:57:02 -0300

    Added Player and Player movement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rigidb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        rigidb.velocity = new Vector2(horizontalInput, rigidb.velocity.y);
+    }
+}
