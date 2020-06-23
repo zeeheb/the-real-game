a9941fdffeb9cc9497bc05fdad9585744a964533
José F <josevictorjop@gmail.com>
2020-06-23 00:56:11 -0300

    Added Animations and some tweaks to the palyer script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,65 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private Rigidbody2D rigidb;
+    private Rigidbody2D rg2d;
+
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+
+    [SerializeField]
+    private bool _grounded = false;
+
+    [SerializeField]
+    private LayerMask _groundLayer;
+
+    private bool resetJumpNeeded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidb = GetComponent<Rigidbody2D>();
+        rg2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        rigidb.velocity = new Vector2(horizontalInput, rigidb.velocity.y);
+        Movement();
+        CheckGrounded();
+
+
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        {
+            rg2d.velocity = new Vector2(rg2d.velocity.x, _jumpForce);
+            _grounded = false;
+            resetJumpNeeded = true;
+            StartCoroutine(ResetJumpNeededRoutine());
+        }
+        rg2d.velocity = new Vector2(move, rg2d.velocity.y);
+    }
+
+    void CheckGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.8f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down * 0.8f, Color.green);
+
+        if (hitInfo.collider != null)
+        {
+            if (resetJumpNeeded == false)
+                _grounded = true;
+        }
+
+    }
+
+    IEnumerator ResetJumpNeededRoutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        resetJumpNeeded = false;
     }
 }
