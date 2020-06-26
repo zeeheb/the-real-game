0895035f594715107c8e53fd99ea29c234f31d05
José F <josevictorjop@gmail.com>
2020-06-25 23:23:01 -0300

    Added Library back???

---
+++ Assets/Scripts/Enemy/Golem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : Enemy
+{
+
+    private void Start()
+    {
+        Attack();
+    }
+
+    public override void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Enemy/Licker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Licker : Enemy
+{
+
+    private void Start()
+    {
+        Attack();
+    }
+
+    public override void Attack()
+    {
+        Debug.Log("Licker attack");
+    }
+
+    public override void Update()
+    {
+
+    }
+}
