b8e534ad6d53f5f83c24661d3ce675e491cdd859
José F <josevictorjop@gmail.com>
2020-06-28 00:26:04 -0300

    Fixed always being grounded

--- Assets/Scripts/Enemy/Licker.cs
+++ Assets/Scripts/Enemy/Licker.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Licker : Enemy
 {
 
+    private Vector3 _currentTarget;
+
     private void Start()
     {
         Attack();
@@ -17,6 +19,22 @@ public class Licker : Enemy
 
     public override void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+
+        }
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
 
     }
 }
